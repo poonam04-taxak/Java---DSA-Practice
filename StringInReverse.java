63a303f98bd8fd8367c5f95bdc810f9b82c83fb9
poonam taxak <poonamtaxak04@gmail.com>
2025-06-15 15:10:54 +0530

    Add files via upload

---
+++ StringInReverse.java
@@ -0,0 +1,15 @@
+public class StringInReverse {
+    public static void printrev(String str,int indx){
+   if(indx==0){
+    System.out.println(str.charAt(indx));
+    return;
+   }
+        System.out.print(str.charAt(indx));
+    printrev(str,indx-1);
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        printrev(str,str.length()-1);
+    }
+    
+}
